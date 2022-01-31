5a320a340220dae4f24fcb0e70966e940d745bd9
CarolShiny <carolinecg431@gmail.com>
2022-01-31 20:27:32 -0300

    Create JogoInexistenteException.java

---
+++ src/ufpb/exercicio5/carol/JogoInexistenteException.java
@@ -0,0 +1,10 @@
+package ufpb.exercicio5.carol;
+
+public class JogoInexistenteException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+    public JogoInexistenteException(String msg){
+        super(msg);
+    }
+
+}
