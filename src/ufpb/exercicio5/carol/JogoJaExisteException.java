846f99f726c68fe1e49b4f0570a9748dc52e59b7
CarolShiny <carolinecg431@gmail.com>
2022-01-31 20:27:44 -0300

    Create JogoJaExisteException.java

---
+++ src/ufpb/exercicio5/carol/JogoJaExisteException.java
@@ -0,0 +1,12 @@
+package ufpb.exercicio5.carol;
+
+public class JogoJaExisteException extends Exception{	
+		private static final long serialVersionUID = 1L;
+
+	    public JogoJaExisteException(String msg){
+	        super(msg);
+	    }
+
+	
+
+}
